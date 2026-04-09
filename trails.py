f4a6a93f6bc67f169198cf9be19e404d6160ea9d
Aly Khimani <aly.khimani8@gmail.com>
2026-04-10 00:28:48 +0100

    Modularised the code

--- trails.py
+++ trails.py
@@ -1,54 +1,65 @@
+import random
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-from configparser import ConfigParser # to read config file
-import random
+from configparser import ConfigParser
 
-config = ConfigParser()
-config.read('config.ini')
+def initialise():
+    config = ConfigParser()
+    config.read('config.ini') # reads in values from config file
 
-grid_size = np.array([int(config.get('trails', 'grid_size_x')), int(config.get('trails', 'grid_size_y'))], dtype=np.uint32)
-nest_loc = np.array([(grid_size[0]*int(config.get('trails', 'nest_loc_x')))//100,
-                     (grid_size[1]*int(config.get('trails', 'nest_loc_y')))//100], dtype=np.uint32)
-ants_int = int(config.get('trails', 'ants_int')) # initial no. of ants
+    grid_size = np.array([int(config.get('trails', 'grid_size_x')), int(config.get('trails', 'grid_size_y'))], dtype=np.uint32)
+    nest_loc = np.array([(grid_size[0]*int(config.get('trails', 'nest_loc_x')))//100,
+                        (grid_size[1]*int(config.get('trails', 'nest_loc_y')))//100], dtype=np.uint32)
+    ants_int = int(config.get('trails', 'ants_int')) # initial no. of ants
 
-grid_marks = np.zeros(shape=grid_size, dtype=np.uint8) # stores locs + levels of markers
-ants_locs = np.full(shape=(ants_int, 2), fill_value=nest_loc, dtype=np.uint32) # start all ants at the nest
+    grid_marks = np.zeros(shape=grid_size, dtype=np.uint8) # stores locs + levels of markers
+    ants_locs = np.full(shape=(ants_int, 2), fill_value=nest_loc, dtype=np.uint32) # start all ants at the nest
 
-grid_food = np.zeros(shape=grid_size, dtype=np.uint8) # 1 = food, 0 = no food
-food_int = int(config.get('trails', 'food_int')) # initial number of food locations
-food_count = 0
-while food_count < food_int: # adds food to random locations
-    x = random.randint(0, grid_size[0]-1)
-    y = random.randint(0, grid_size[1]-1)
-    if grid_food[x, y] == 0 and (x != nest_loc[0] or y != nest_loc[1]): # ensures location has no food and is not the nest
-        grid_food[x, y] = 1
-        food_count += 1
+    """grid_food = np.zeros(shape=grid_size, dtype=np.uint8) # 1 = food, 0 = no food
+    food_int = int(config.get('trails', 'food_int')) # initial number of food locations
+    food_count = 0
+    while food_count < food_int: # adds food to random locations
+        x = random.randint(0, grid_size[0]-1)
+        y = random.randint(0, grid_size[1]-1)
+        if grid_food[x, y] == 0 and (x != nest_loc[0] or y != nest_loc[1]): # ensures location has no food and is not the nest
+            grid_food[x, y] = 1
+            food_count += 1"""
+    
+    return grid_size, nest_loc, ants_int, grid_marks, ants_locs
 
-fig, ax = plt.subplots()
-ax.set_xlim(0, grid_size[1]) # matplotlib swaps x and y axes
-ax.set_ylim(0, grid_size[0])
+def grid(grid_size, nest_loc, ants_int, grid_marks, ants_locs):
+    fig, ax = plt.subplots()
+    ax.set_xlim(0, grid_size[1]) # matplotlib swaps x and y axes
+    ax.set_ylim(0, grid_size[0])
 
-food_rows, food_cols = np.where(grid_food == 1) # finds all the food 
-ax.scatter(food_cols, food_rows, color='red', marker='x', s=50, label='Food') # plots food locations
-ax.scatter(nest_loc[1], nest_loc[0], color='b', marker='x', s=100, linewidth=2) # plots the nest location
+    #food_rows, food_cols = np.where(grid_food == 1) # finds all the food 
+    #ax.scatter(food_cols, food_rows, color='red', marker='x', s=50, label='Food') # plots food locations
+    ax.scatter(nest_loc[1], nest_loc[0], color='b', marker='x', s=100, linewidth=2) # plots the nest location
 
-marks_plot = ax.imshow(np.zeros(grid_size), cmap='Greens', alpha=0.7, vmin=0, vmax=ants_int//2) # spaces with many markers will be darkest green
-def update(frame): # moves ants by a simple random walk
-    for ant in range(ants_int): # moves ants 1 step in a random direction but keeps it on the grid
-        ants_locs[ant, 0] = min(max(0, ants_locs[ant, 0] + random.randint(-1, 1)), grid_size[0]-1)
-        ants_locs[ant, 1] = min(max(0, ants_locs[ant, 1] + random.randint(-1, 1)), grid_size[1]-1)
+    def format_coord(x, y): # changes the coordinate readout to integers (shows when hovering over the grid)
+        return 'x =% 2.0f, y =% 2.0f' % (x, y)
+    ax.format_coord = format_coord
 
-        grid_marks[ants_locs[ant, 0], ants_locs[ant, 1]] += 1 # adds marker
+    fig.suptitle('Ant Simulation', fontweight ="bold")
 
-    marks_plot.set_data(np.ma.masked_where(grid_marks.T == 0, grid_marks.T)) # only updates changes to grid_markers
-    return [marks_plot]
+    marks_plot = ax.imshow(np.zeros(grid_size), cmap='Greens', alpha=0.7, vmin=0, vmax=ants_int//2) # spaces with many markers will be darkest green
+    def update(frame): # moves ants by a simple random walk
+        for ant in range(ants_int): # moves ants 1 step in a random direction but keeps it on the grid
+            ants_locs[ant, 0] = min(max(0, ants_locs[ant, 0] + random.randint(-1, 1)), grid_size[0]-1)
+            ants_locs[ant, 1] = min(max(0, ants_locs[ant, 1] + random.randint(-1, 1)), grid_size[1]-1)
 
-ani = animation.FuncAnimation(fig, update, frames=200, interval=50, blit=True) # 50ms between frames
+            grid_marks[ants_locs[ant, 0], ants_locs[ant, 1]] += 1 # adds marker
 
-def format_coord(x, y): # changes the coordinate readout to integers when hovering
-    return 'x =% 2.0f, y =% 2.0f' % (x, y)
-ax.format_coord = format_coord
+        marks_plot.set_data(np.ma.masked_where(grid_marks.T == 0, grid_marks.T)) # only updates changes to grid_markers
+        return [marks_plot]
 
-fig.suptitle('Ant Simulation', fontweight ="bold")
-plt.show()
+    ani = animation.FuncAnimation(fig, update, frames=200, interval=50, blit=True) # 50ms between frames
+    plt.show()
+
+def main():
+    grid_size, nest_loc, ants_int, grid_marks, ants_locs = initialise()
+
+    grid(grid_size, nest_loc, ants_int, grid_marks, ants_locs)
+
+main()
